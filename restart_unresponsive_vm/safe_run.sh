ca042188527402470ab71e846ed3ad781d8282b2
sergio <sergioatamorim@gmail.com>
2020-05-25 21:26:09 -0300

    improvements on the detection of host responsiveness
    - added logic to differ if the host ip is set (by vmtools) and if not, wait some time checking if it is set before continuing;\n- changed reboot command to reset because reboot requires vmtools to be running;\n- added sleeping after reseting vm to prevent reseting a vm that is not yet started.

--- restart_unresponsive_vm/safe_run.sh
+++ restart_unresponsive_vm/safe_run.sh
@@ -2,8 +2,8 @@
 
 # show usage and exit
 show_usage() {
-   echo "Usage: $0 -v <vmid> -t <timeout per try> -c <quantity of tries> -l <log path>"
-   exit 1
+  echo "Usage: $0 -v <vmid> -t <timeout per try> -c <quantity of tries> -l <log path>"
+  exit 1
 }
 
 # get values from opts
@@ -21,7 +21,7 @@ done
 # show usage if some variable is not set
 if [ -z "$vmid" ] || [ -z "$timeout" ] || [ -z "$count" ] || [ -z "$log_path" ]
 then
-   show_usage
+  show_usage
 fi
 
 # limiting size to 2MB (+2MB of archive)
@@ -32,6 +32,14 @@ fi
 
 # check if host is answers ping
 host=$(/bin/vim-cmd vmsvc/get.guest $vmid | /bin/grep -m 1 "ipAddress = " | /bin/sed 's/   ipAddress = "//' | /bin/sed 's/", //')
+index=1
+# if vmtools is not running (as when the vm is yet starting) the ipAddress will be shown as unset;
+# this loop tries to get the vm ip in a similar way the next loop tries to get a sucessful icmp response (and similar time to run)
+# if no ip is found the next loop will fail as well and the vm will be restarted
+while [[ $host -eq "   ipAddress = <unset>, " && $index -lt $count ]]; do
+  host=$(/bin/vim-cmd vmsvc/get.guest $vmid | /bin/grep -m 1 "ipAddress = " | /bin/sed 's/   ipAddress = "//' | /bin/sed 's/", //')
+  /bin/sleep $timeout
+done
 /bin/ping -c 1 -W $timeout $host &>/dev/null
 ping=$?
 index=1
@@ -46,5 +54,7 @@ NOW=$(/bin/date +"%Y-%m-%d %H:%M:%S")
 if [ $ping -eq 0 ]; then /bin/echo "$NOW - ${0##*/}: $host online" >> $log_path
 else
   /bin/echo "$NOW - ${0##*/}: $host offline - restarting" >> $log_path
-  /bin/vim-cmd vmsvc/power.reboot $vmid >> $log_path 2>&1
+  # the reset command is used instead of reboot because reboot requires vmtools running (and responding) on the vm
+  /bin/vim-cmd vmsvc/power.reset $vmid >> $log_path 2>&1
+  /bin/sleep $(($timeout*$count))  # prevents reseting the vm when its starting after being reset
 fi
